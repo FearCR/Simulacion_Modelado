175d044cc07b29acb4653215f338e9c13afaf699
ALEJANDRO DUARTE LOBO <alejandro.duarte@ucr.ac.cr>
2020-06-20 16:14:31 -0600

    main

--- Simulacion.py
+++ Simulacion.py
@@ -1,5 +1,22 @@
+MAX_VALUE = 999999999
+clock = 0
+time_to_finish = 5000
+queue_s1 = 0
+queue_s2 = 0
+s1_server1 = False
+s2_server1 = False
+s2_server2 = False
+events = [MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE]
+
+def data_init(e1):
+	events[0]=e1
+
 def get_next_event(events):
-	return 0
+	next_event=0
+	for i in range (len(events)):
+		if events[i]<events[next_event]:
+			next_event=i
+	return next_event
 
 def event_one():
 	return
@@ -16,27 +33,21 @@ def event_six():
 def event_seven():
 	return
 
-MAX_VALUE = 999999999
-clock = 0
-time_to_finish = 5000
-queue_s1 = []
-queue_s2 = []
-s1_server1 = False
-s2_server1 = False
-s2_server2 = False
-events = [MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE]
+def main():
+	data_init(3)
+	while clock < time_to_finish:
+		event = get_next_event(events)
+		switcher = {
+			0:event_one,
+			1:event_two,
+			2:event_three,
+			3:event_four,
+			4:event_five,
+			5:event_six,
+			6:event_seven
+		}
+		func = switcher.get(event, "invalid event")
 
-while clock < time_to_finish:
-	event = get_next_event(events)
-	switcher = {
-		0:event_one,
-		1:event_two,
-		2:event_three,
-		3:event_four,
-		4:event_five,
-		5:event_six,
-		6:event_seven
-	}
-	func = switcher.get(event, "invalid event")
-	clock = time_to_finish
-	uy = func()
+
+if __name__ == "__main__":
+    main()

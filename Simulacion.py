9fe1e0fd23596d37c1af1a4966b67ee12ff4c9ec
Isacc Zúñiga <isaack9734@gmail.com>
2020-06-24 10:56:13 -0600

    distribuciones

--- Simulacion.py
+++ Simulacion.py
@@ -1,22 +1,29 @@
-
 from random import random
 from random import randrange
 import math
 
 MAX_VALUE = 999999999
+TIME_TO_FINISH = 5000+120
 clock = 0
-time_to_finish = 5000
 queue_s1 = 0
 queue_s2 = 0
 s1_server1 = False
 s2_server1 = False
 s2_server2 = False
-events = [MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE,MAX_VALUE]
+paquetesListos=0
+mascarillasDesechadas=0
 
+
+distributions = [-1,-1,-1,-1]
+
+events = [[MAX_VALUE],[],[],[MAX_VALUE],[],[MAX_VALUE],[MAX_VALUE]]
+
+
+#distribuciones
 def uniforme(a,b):
     r=random()
     x=(b-a)*r+a
-    return int(x)
+    return x
 
 def normal(Mu,vari):
     r1 = random()
@@ -28,12 +35,12 @@ def normal(Mu,vari):
     x=(vari*Z)+Mu
     if x<0:
         x=-1*x
-    return int(x)
+    return x+1
 
 def exponencial(lamb):
     r = random()
     x=(-math.log(1-r))/lamb
-    return int(x)
+    return x
 
 def convolucion(Mu,vari):
     Z=0
@@ -41,130 +48,263 @@ def convolucion(Mu,vari):
         r = random()
         Z=Z+r
     x = (vari * Z) + Mu
-    return int(x)
+    return x
 
 def funcionDensidad(funcion,a,b):
     print("por implementar")
 
 
 
-
+#Generacion de numeros aleatoreos con las distribuciones deseadas
 def generate_d1():
-	return 0
+	if distributions[0] == 1:
+		return uniforme(1,3)
+	else:
+		if distributions[0] == 2:
+			return normal(4,5)
+		else:
+			if distributions[0] == 3:
+				return exponencial(4)
+			else:
+				return convolucion(1,9)
 
 def generate_d2():
-	return 0
+	if distributions[0] == 1:
+		return uniforme(1,3)
+	else:
+		if distributions[0] == 2:
+			return normal(4,5)
+		else:
+			if distributions[0] == 3:
+				return exponencial(4)
+			else:
+				return convolucion(1,9)
 
 def generate_d3():
-	return 0
+	if distributions[0] == 1:
+		return uniforme(1,3)
+	else:
+		if distributions[0] == 2:
+			return normal(4,5)
+		else:
+			if distributions[0] == 3:
+				return exponencial(4)
+			else:
+				return convolucion(1,9)
 
 def generate_d4():
-	return 0
+	if distributions[0] == 1:
+		return uniforme(1,3)
+	else:
+		if distributions[0] == 2:
+			return normal(4,5)
+		else:
+			if distributions[0] == 3:
+				return exponencial(4)
+			else:
+				return convolucion(1,9)
 
 
-#llega una mascarilla del exterior a la seccion 1
+#llega mascarilla del exterior a Seccion 1
 def event_one():
-	global s1_server1
-	global events
-	global queue_s1
-	clock = events[0]
-	if s1_server1 == False:
-		s1_server1 = True
-		d2 = generate_d2()
-		events[3] = int(clock) + int(d2)
-		print(s1_server1)
-	else:
-		queue_s1 = queue_s1 + 1
-		d1 = generate_d1()
-		events[0] = int(clock) + int(d1)
-		print(s1_server1)
-	return
-def event_two():
-	return
-def event_three():
-	return
+    global clock
+    global s1_server1
+    global events
+    global queue_s1
+    clock = events[0][0]
+    print("e1",events,clock)
+    if s1_server1 == False:
+        s1_server1 = True
+        d2 = generate_d2()
+        events[3][0] = clock + d2
+    else:
+        queue_s1 = queue_s1 + 1
+        d1 = generate_d1()
+        events[0][0] = clock + d1
+        return
 
-#Se desocupa el servidor de la seccion 1
+#llegan 2 mascarillas de la seccion 2 servidor1
+def event_two():
+    global clock
+    global s1_server1
+    global events
+    global queue_s1
+    clock=events[1].pop(0)
+    print("e2",events,clock)
+    if s1_server1 == False:
+        queue_s1 = queue_s1 + 1
+        s1_server1==True
+        d2 = generate_d2()
+        events[3][0] = clock + d2
+        print(s1_server1)
+    else:
+        queue_s1 = queue_s1 + 2
+        print(s1_server1)
+    return
+
+#llegan 2 mascarillas de la seccion 2 servidor2
+def event_three():
+    global s1_server1
+    global events
+    global queue_s1
+    global clock
+    clock = events[2].pop(0)
+    print("e3",events,clock)
+    if s1_server1 == False:
+        s1_server1 = True
+        queue_s1 = queue_s1 + 1
+        d2 = generate_d2()
+        events[3][0] = clock + d2
+        print(s1_server1)
+    else:
+        queue_s1 = queue_s1 + 2
+    return
+
+#se desocupa la seccion 1
 def event_four():
-	global queue_s1
-	global events
-	global MAX_VALUE
-	global s1_server1
-	global clock
-	clock = events[3]
-	if queue_s1 > 0:
-		queue_s1 = queue_s1 - 1
-		d2 = generate_d2()
-		events[3] = int(clock) + int(d2)
-	else:
-		events[3] = MAX_VALUE
-		s1_server1 = False
-	random_value = randrange(100)
-	if random_value >= 10:			#el 90% de las veces no se desecha y se programa el evento 5
-		events[4] = clock + 1
-	return
+    global clock
+    global mascarillasDesechadas
+    global queue_s1
+    global events
+    global MAX_VALUE
+    global s1_server
+    clock = events[3][0]
+    print("e4",events,clock)
+    if queue_s1 > 0:
+        queue_s1 = queue_s1 - 1
+        d2 = generate_d2()
+        events[3][0] = clock + d2
+    else:
+        events[3][0] = MAX_VALUE
+        s1_server1 = False
+    random_value = randrange(100)
+    if random_value > 10:			#el 90% de las veces no se desecha y se programa el evento 5
+        events[4].append(clock + 1)
+    else:
+        mascarillasDesechadas=mascarillasDesechadas+1
+    return
 
 #llega una mascarilla a la seccion 2
 def event_five():
-	global clock
-	global s2_server1
-	global s2_server2
-	global queue_s2
-	clock = events[4]
-	if queue_s2 >= 1:
-		if s2_server1 == False | s2_server2 == False:
-			if s2_server1 == False:
-				queue_s2 = queue_s2 - 1
-				d3 = generate_d3()
-				events[5] = int(clock) + int(d3)
-				s2_server1 = True
-			else:
+    global clock
+    global events
+    global s2_server1
+    global s2_server2
+    global queue_s2
+    clock = events[4].pop(0)
+    print("e5",events,clock)
+    if queue_s2 >= 1:
+        if s2_server1 == False | s2_server2 == False:
+            if s2_server1 == False:
+                queue_s2 = queue_s2 - 1
+                d3 = generate_d3()
+                events[5][0] = clock + d3
+                s2_server1 = True
+            else:
 				#if s2_server2 == False:
-				queue_s2 = queue_s2 - 1
-				d4 = generate_d4()
-				events[6] = int(clock) + int(d4)
-				s2_server2 = True
-		else:
-			queue_s2 = queue_s2 + 1
-	else:
-		queue_s2 = queue_s2 + 1
-	return
+                queue_s2 = queue_s2 - 1
+                d4 = generate_d4()
+                events[6][0] = clock + d4
+                s2_server2 = True
+        else:
+            queue_s2 = queue_s2 + 1
+    else:
+        queue_s2 = queue_s2 + 1
+    return
+
+#se desocupa el servidor 1 de la seccion 2
 def event_six():
-	return
+    global events
+    global MAX_VALUE
+    global s2_server1
+    global queue_s2
+    global paquetesListos
+    global mascarillasDesechadas
+    global clock
+    s2_server1 = False
+    clock = events[5][0]
+    print("e6",events,clock)
+    if queue_s2 >= 2:
+        queue_s2 = queue_s2 - 2
+        d3 = generate_d3()
+        events[5][0] = clock + d3
+    else:
+        events[5][0] = MAX_VALUE
+        s2_server1 = False
+    random_value = randrange(100)
+    if random_value >= 20 and random_value<75:
+        events[1].append(clock + 2)
+    elif random_value >= 5 and random_value<20:
+        mascarillasDesechadas=mascarillasDesechadas+2
+    elif random_value >= 75:
+        paquetesListos=paquetesListos+1
+    return
+
 
 #se desocupa el servidor 2 de la seccion 2
 def event_seven():
-	global clock
-	global queue_s2
-	global s2_server2
-	clock = events[6]
-	if queue_s2 >= 2:
-		queue_s2 = queue_s2 - 2
-		d4 = generate_d4()
-		events[6] = int(clock) + int(d4)
-	else:
-		events[6] = MAX_VALUE
-		s2_server2 = False
-	random_value = randrange(100)
-	if random_value >= 15 and random_value < 40:
-		events[2] = int(clock) + 2 
-	return
+    global clock
+    global queue_s2
+    global s2_server2
+    global paquetesListos
+    global mascarillasDesechadas
+    clock = events[6][0]
+    print("e7",events,clock)
+    if queue_s2 >= 2:
+        queue_s2 = queue_s2 - 2
+        d4 = generate_d4()
+        events[6][0] = clock + d4
+    else:
+        events[6][0] = MAX_VALUE
+        s2_server2 = False
+    random_value = randrange(100)
+    if random_value >= 15 and random_value < 40:
+        events[3].append(clock + 2)
+    elif random_value >= 15 and random_value < 25:
+        mascarillasDesechadas=mascarillasDesechadas+2
+    elif random_value>=40:
+        paquetesListos=paquetesListos+1
+    return
 
+#metodo para inicializar datos para iniciar la simulacion
 def data_init(e1):
-	events[0]=e1
+	events[0][0]=e1
 
+#metodo para buscar el evento mas proximo
 def get_next_event(events):
-	next_event=0
-	for i in range (len(events)):
-		if events[i]<events[next_event]:
-			next_event=i
-	return next_event
+    next_event=0
+    for i in range (len(events)):
+        if len(events[i])>0:
+            if events[i][0]<events[next_event][0]:
+                next_event=i
+    return next_event
+
 def main():
-	global clock
-	data_init(3)
-	while clock < time_to_finish:
-		event = get_next_event(events)
-		switcher = {
+    global clock
+    global events
+    global MAX_VALUE
+    global s1_server1
+    global s2_server1
+    global s2_server2
+    global queue_s1
+    global queue_s2
+    global paquetesListos
+    global mascarillasDesechadas
+    global distributions
+    
+    print("seleccione cada una de las distribuciones que desea utilizar : ")
+    distribution = 0
+    while distribution < 4:
+		distributions[distribution] = input("1 : Uniforme - 2: Normal  - 3 : Exponencial - 4 : Convolucion  : \n")
+		distribution = distribution + 1
+	
+    
+      
+    
+    data_init(3)
+    while clock < TIME_TO_FINISH:
+        event = get_next_event(events)
+        switcher = {
 			0:event_one,
 			1:event_two,
 			2:event_three,
@@ -173,10 +313,15 @@ def main():
 			5:event_six,
 			6:event_seven
 		}
-		func = switcher.get(event, "invalid event")
-		clock=time_to_finish
-		print(normal(25,100))
-		print(randrange(100))
+        func = switcher.get(event, "invalid event")
+        func()
+        #clock=TIME_TO_FINISH
+        #print(normal(2,10))
+        #print(randrange(100))
+
+
+
 
 if __name__ == "__main__":
     main()
+

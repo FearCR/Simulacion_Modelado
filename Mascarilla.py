4a7e3a2b70104fbe17570776a6a62333ec77c35b
Isacc Zúñiga <isaack9734@gmail.com>
2020-07-01 10:36:51 -0600

    time masks

--- Mascarilla.py
+++ Mascarilla.py
@@ -2,6 +2,13 @@ class mascarilla:
     def __init__(self):
         self.tiempoEncola=0
         self.tiempoDeSalida=0
+        self.initial_clock= 0
+     
+    def set_initial_clock(self, clock):
+        self.set_initial_clock = clock
+
+    def get_initial_clock(self, clock):
+        return self.initial_clock
 
     def setTiempoEncola(self,tiempo):
         self.tiempoEncola=  self.tiempoEncola+tiempo

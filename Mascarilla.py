4ced99b4ce8f2f12dec8568c49f92893b5ef390a
Zahard-TOG <65728890+Zahard-TOG@users.noreply.github.com>
2020-07-11 19:34:40 -0600

    Documentacion de mascarilla, mas eliminación de métodos que no se estaban usando

--- Mascarilla.py
+++ Mascarilla.py
@@ -1,23 +1,23 @@
 class mascarilla:
     def __init__(self):
-        self.tiempoEncola = 0
-        self.tiempoDeSalida = 0
+
         self.initial_clock = 0
 
+    '''
+    @Descripcion: Se encarga de colocar en "self.initial_clock", el tiempo 
+    en que la mascarilla entro al sistema.
+    @utiliza: self.initial_clock
+    @modifica: self.initial_clock
+    '''
     def set_initial_clock(self, clock):
         self.initial_clock = clock
 
+    '''
+    @Descripcion: Se encarga de retornar el tiempo 
+    en que la mascarilla entro al sistema.
+    @utiliza: self.initial_clock
+    @modifica: Nada
+    '''
     def get_initial_clock(self):
         return self.initial_clock
 
-    def setTiempoEncola(self, tiempo):
-        self.tiempoEncola = self.tiempoEncola + tiempo
-
-    def getTiempoEncola(self):
-        return self.tiempoEncola
-
-    def setTiempoDeSalida(self, tiempo):
-        self.tiempoDeSalida = self.tiempoDeSalida + tiempo
-
-    def getTiempoDeSalida(self):
-        return self.tiempoDeSalidaalida
